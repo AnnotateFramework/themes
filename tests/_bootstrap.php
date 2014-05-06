d29676d4bce492821535f16618195eb5f7e343d7
Michal Vysinsky <vysinsky@live.com>
2014-05-06 20:43:29 +0200

    fixed tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,13 +1,6 @@
 <?php
 
-$projectPath = __DIR__ . '/../../../autoload.php';
-
-if (file_exists($projectPath)) {
-    require_once $projectPath;
-} else {
-    require_once __DIR__ . '/../vendor/autoload.php';
-}
-
+require __DIR__ . '/../vendor/autoload.php';
 define('ROOT_DIR', __DIR__ . '/');
 define('DATA_DIR', __DIR__ . '/_data');
 define('TEMP_DIR', DATA_DIR . '/tmp');
